1456cc4453b922b32bd4e207a54f13caed1dd752
Gerhard VAN-ROOYEN <gvan-roo@e4r5p7.wtc.co.za>
2017-08-19 17:16:37 +0200

    updated working label_offset.c

--- label_offset.c
+++ label_offset.c
@@ -6,9 +6,11 @@
 /*   By: gvan-roo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/19 10:33:24 by gvan-roo          #+#    #+#             */
-/*   Updated: 2017/08/19 11:05:27 by gvan-roo         ###   ########.fr       */
+/*   Updated: 2017/08/19 17:15:15 by gvan-roo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "../headers/asm.h"
 
 /*
 **	Counts the number of bytes each argument takes, and modifies the bytes
@@ -17,31 +19,39 @@
 **
 **	Variable ctr starts at 1, as lst-data[0] will always be an opcode which
 **	takes 1 byte.
-**
-**	Variable tot_bytes starts at 1, as the opcode is already taken into account
 */
 void			label_offset(t_prog *lst)
 {
 	int			ctr;
 	int			tot_bytes;
 
-	tot_bytes = 1;
+	tot_bytes = 0;
 	while (lst)
 	{
-		ctr = 1;
-		if (ft_strcmp(lst->data[0], "live") == 0)
+		ctr = 0;
+		if (lst->data)
 		{
-			tot_bytes += 4;
-		}
-		while (lst->data[ctr])
-		{
-			if (lst->data[ctr][0] == 'r')
-				tot_bytes += 1;
-			else if (lst->data[ctr][0] == '%')
-				tot_bytes += 2;
-			else
-				tot_bytes += 4;
-			ctr++;
+			while (lst->data[ctr])
+			{
+				if (ft_strcmp(lst->data[ctr], "live") == 0)
+				{
+					tot_bytes += 5;
+					ctr += 2;
+					continue ;
+				}
+				else if (ctr == 0)
+				{
+					ctr++;
+					tot_bytes++;
+				}
+				if (lst->data[ctr][0] == 'r')
+					tot_bytes += 1;
+				else if (lst->data[ctr][0] == '%')
+					tot_bytes += 2;
+				else
+					tot_bytes += 4;
+				ctr++;
+			}
 		}
 		lst->bytes = tot_bytes;
 		lst = lst->next;
