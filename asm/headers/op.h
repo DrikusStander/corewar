4453f42c6ca5d01df0ae7660ab6ab80a6875fb35
Hendrik STANDER <hstander@e4r5p9.wtc.co.za>
2017-08-22 10:53:27 +0200

    added op_tab struct to op.h

--- asm/headers/op.h
+++ asm/headers/op.h
@@ -6,24 +6,17 @@
 /*   By: zaz <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2017/08/14 16:02:09 by hstander         ###   ########.fr       */
+/*   Updated: 2017/08/22 10:52:41 by hstander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-/*
- * ** Toutes les tailles sont en octets.
- * ** On part du principe qu'un int fait 32 bits. Est-ce vrai chez vous ?
- * */
 
 #define IND_SIZE				2
 #define REG_SIZE				4
 #define DIR_SIZE				REG_SIZE
 
-
 # define REG_CODE				1
 # define DIR_CODE				2
 # define IND_CODE				3
-
 
 #define MAX_ARGS_NUMBER			4
 #define MAX_PLAYERS				4
@@ -48,29 +41,35 @@
 #define NBR_LIVE				21
 #define MAX_CHECKS				10
 
-/*
- * **
- * */
-
-typedef char	t_arg_type;
+typedef char			t_arg_type;
 
 #define T_REG					1
 #define T_DIR					2
 #define T_IND					4
 #define T_LAB					8
 
-/*
- * **
- * */
-
 # define PROG_NAME_LENGTH		(128)
 # define COMMENT_LENGTH			(2048)
 # define COREWAR_EXEC_MAGIC		0xea83f3
 
-typedef struct		header_s
+typedef struct			header_s
 {
 	unsigned int		magic;
 	char				prog_name[PROG_NAME_LENGTH + 1];
 	unsigned int		prog_size;
 	char				comment[COMMENT_LENGTH + 1];
-}					header_t;
+}						header_t;
+
+typedef struct			s_op
+{
+	char				*name;
+	int					no_args;
+	char				type[MAX_ARGS_NUMBER];
+	char				id;
+	int					no_cyles;
+	char				*desc;
+	int					has_acb;
+	int					is_index;
+}						t_op;
+
+extern	t_op			g_op_tab[];
