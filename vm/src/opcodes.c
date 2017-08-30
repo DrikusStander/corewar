2b1531d89eb4f68ab4bbf14ba56f377120698d29
Hendrik STANDER <hstander@e5r2p8.wethinkcode.co.za>
2017-08-30 10:12:41 +0200

    new

--- vm/src/opcodes.c
+++ vm/src/opcodes.c
@@ -6,7 +6,7 @@
 /*   By: gvan-roo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/29 16:20:28 by gvan-roo          #+#    #+#             */
-/*   Updated: 2017/08/30 10:06:22 by hstander         ###   ########.fr       */
+/*   Updated: 2017/08/30 10:11:31 by hstander         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void			ft_ld(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 /*
 void			ft_st(t_vm *vm, t_champ *champ)
@@ -28,76 +29,89 @@ void			ft_add(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_sub(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_and(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_or(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_xor(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_zjmp(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_ldi(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_sti(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_fork(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_lld(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_lldi(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_lfork(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
 
 void			ft_aff(t_vm *vm, t_champ *champ)
 {
 	(void)vm;
 	(void)champ;
+	champ->exec_cycle = g_op_tab[2].no_cycles;
 }
