4de19c8e66726cfc4723159b9be05b09de23e7a5
Drikus <Drikus@Drikuss-Mac.local>
2017-09-10 12:03:24 +0200

    fixed st.c where it wrote to the wrong mem and changed dump cycle values

--- ops/st.c
+++ ops/st.c
@@ -17,13 +17,13 @@ static void	ft_to_mem(t_vm *vm, t_champ *champ, int arg2, int arg1)
 	int		c_pc;
 
 	c_pc = champ->pc;
-	vm->mem[c_pc + (arg2 % IDX_MOD)] = (arg1 & 0xff000000) >> 24;
+	vm->mem[mem_check(c_pc + (arg2 % IDX_MOD))] = (arg1 & 0xff000000) >> 24;
 	c_pc++;
-	vm->mem[c_pc + (arg2 % IDX_MOD)] = (arg1 & 0x00ff0000) >> 16;
+	vm->mem[mem_check(c_pc + (arg2 % IDX_MOD))] = (arg1 & 0x00ff0000) >> 16;
 	c_pc++;
-	vm->mem[c_pc + (arg2 % IDX_MOD)] = (arg1 & 0x0000ff00) >> 8;
+	vm->mem[mem_check(c_pc + (arg2 % IDX_MOD))] = (arg1 & 0x0000ff00) >> 8;
 	c_pc++;
-	vm->mem[c_pc + (arg2 % IDX_MOD)] = (arg1 & 0x000000ff);
+	vm->mem[mem_check(c_pc + (arg2 % IDX_MOD))] = (arg1 & 0x000000ff);
 }
 
 void		ft_st(t_vm *vm, t_champ *champ)
